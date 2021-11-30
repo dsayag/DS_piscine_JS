c50037cfb80d930aeede0ddbeb235c8a4502a98d
dsayag <d.sayag@it-students.fr>
2021-11-30 11:40:23 +0100

    Day01 - Ex00

---
+++ Day01/ex00/program.js
@@ -0,0 +1,30 @@
+let array = [10, 8, 45, 7, 15];
+//let array = [];
+
+function tabTriCarre(array) {
+    let calcul;
+    let newArray = [];
+    array.sort(function (a, b) { return a - b });
+
+    for (let i = 0; i < array.length; i++) {
+        calcul = array[i] * array[i];
+        newArray.push(calcul);
+    }
+
+    console.log(newArray);
+
+
+    if (array == ![]) {
+        console.log('Le tableau est vide');
+    } else {
+        console.log("Le tableau n'est pas vide, ses valeurs sont : " + newArray);
+    }
+
+    return newArray;
+
+}
+
+// Merci de ne pas effacer la ligne en dessous.
+//exports.tabTriCarre =  tabTriCarre;
+
+let result = tabTriCarre(array);
