c86e0e9239787184dda82f369c7a4ca9f51a8cc3
dsayag <d.sayag@it-students.fr>
2021-12-01 09:30:27 +0100

    MAJ Day01 ex03

--- Day01/ex03/program.js
+++ Day01/ex03/program.js
@@ -5,16 +5,10 @@ function runLengthEncoding(str) {
         let n = str.length;
         for (let i = 0; i < n; i++) { // Compter le nbre de caractères -> 20
             //console.log("nombre de caractère : " + n);
-            //console.log("caractère i : " + str[i]); // Affiche les caractères
-
-            let count = 0;
-            while (i < n - 1 && str[i] == str[i + 1]) {
-                count++;
-                i++;
-            }
+            //console.log("caractère i : " + str[i]); // Affiche les caractères            
 
             console.log(str[i]); // Print character and its count
-            console.log(count);
+            //console.log(count);
         }
     }
 }
